c95aedcc3b34bf3d5b7f0d4faed11a4c3679cf39
Nelia Terrazas and Ryan McLean <ryan@>
2019-01-17 11:09:50 -0800

    finished if/then and js/css

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,26 @@ $(document).ready(function() {
   $("#enterHeight").submit(function(event) {
     var height = parseInt($("#yourHeight").val());
     if(height >= 48) {
-      $('.tall').show();
+      $('#tall').show();
+      $(".extremeRides").show();
+      $('#short').hide();
+      $(".smallRides").hide();
     } else {
-      $('.short').show()
+      $('#short').show();
+      $(".smallRides").show();
+      $('#tall').hide();
+      $(".extremeRides").hide();
     }
-    
+
     event.preventDefault();
+
+  });
+
+  $("#allRides").click(function() {
+    $(".extremeRides").slideDown();
+    $(".smallRides").slideDown();
+    $("#short").hide();
+    $("#tall").hide();
+
   });
 });
